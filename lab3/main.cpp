e2a8cc74daaa27b5f528628a9e4c6335c3dac464
Dustin Horvath <dustinmhorvath@gmail.com>
2016-02-11 11:17:17 -0600

    Pretty much finished. Might add some mechanisms for averaging the times recorded.

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -32,30 +32,43 @@ double* getRandomArray(int size, int l_bound, int u_bound){
 int main(){
 
   Timer timer = Timer();
-  srand (time(NULL));
+  srand (20);
 
 
-  int size = 10;
+  int size = 200;
+  int l_bound = -40000;
+  int u_bound = 40000;
   double* arr = new double[size];
   int iterations = 10;
 
-  std::cout << "Running " << iterations << " iterations of insertion sort...\n";
+  std::cout << "Running " << iterations << " iterations of insertion sort with size " << size << "...\n";
+  for(int i = 0; i < iterations; i++){
+    arr = getRandomArray(size, l_bound, u_bound);
+    
+    timer.start();
+    insert_sort(arr, size);
+    timer.printTime(timer.stop());
+    /*
+    for(int i = 0; i < size; i++){
+      std::cout << arr[i] << " ";
+    }
+    */
+  }
+
+  size = 400000;
+  l_bound = -80000;
+  u_bound = 80000;
+  
+  std::cout << "Running " << iterations << " iterations of insertion sort with size " << size << "...\n";
   for(int i = 0; i < iterations; i++){
     arr = getRandomArray(size, -40000, 40000);
     
     timer.start();
     insert_sort(arr, size);
     timer.printTime(timer.stop());
-    
-    for(int i = 0; i < size; i++){
-      std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
   }
 
   std::cout << "Exiting...\n";
-
-
 
   return 0;
 }
