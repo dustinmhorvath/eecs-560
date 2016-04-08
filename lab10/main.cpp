4facbb0c65c9e36607e280de8fca234815393919
Dustin Horvath <dustinmhorvath@gmail.com>
2016-04-08 15:57:15 -0500

    Coming along. Still missing an important 'find' somewhere.

--- lab10/main.cpp
+++ lab10/main.cpp
@@ -9,125 +9,178 @@
 #include <math.h>
 // used for pretty output
 #include <iomanip>
+#include <vector>
 
+class DisjSets{
 
+public:
+  DisjSets(int width, int height){
+    m_width = width;
+    m_height = height;
+    numconnected = m_width*(m_height);
+    connected = new int[numconnected];
+    for(int i = 0; i < numconnected; i++){
+      connected[i]= -1;
+    }
 
+    numvert = (m_width-1)*m_height;
+    vertical = new int[numvert];
+    m_width*(m_height-1);
+    for(int i = 0; i < numvert; i++){
+      vertical[i]= 0;
+    }
 
-int main(){
+    numhor = m_width*(m_height-1);
+    horizontal = new int[numhor];
+    for(int i = 0; i < numhor; i++){
+      horizontal[i]= 0;
+    }
 
-  int seed = 0;
-  srand (seed);
-
-  clock_t t0;
-  t0 = clock();
-
-  int width = 5;
-  int height = 5;
-
-  int numvert = (width-1)*height;
-  int vertical[numvert];width*(height-1); //should initialize as zeroes
-  for(int i = 0; i < numvert; i++){
-    vertical[i]= 0;
   }
 
-  int numhor = width*(height-1);
-  int horizontal[numhor]; //should initialize as zeroes
-  for(int i = 0; i < numhor; i++){
-    horizontal[i]= 0;
+  void unionSets(int root1, int root2){
+    if(connected[root2] < connected[root1]){
+      connected[root1] = root2;
+    }
+    else{
+      if(connected[root1] == connected[root2]){
+        --connected[root1];
+      }
+      connected[root2] = root1;
+    }
   }
 
-  int numconnected = width*(height);
-  int connected[numconnected];
-  for(int i = 0; i < numconnected; i++){
-    connected[i]= -1;
-  }
-
-  int count = 0;
-  int num = 50;
-  //int cell = 0;
-  //for(int node = 0; node < numconnected; node++){
-  while(count < num){
-    //cell = node;
-    // Randomly select a cell
-    int cell = rand()%(width*height);
-    std::cout << "Cell: " << cell << "\n";
-    // Check if it's connected already
-    if(connected[cell] == -1){
-      // Randomly select a wall
+  void buildmaze(){
+    
+    for(int i = 0; i < numconnected-1; i++){
       int wall = rand()%4;
-      std::cout << "Wall: " << wall << "\n\n";
+      int cell = i;
       // North
-      if(wall == 0 && cell >= width){
-        connected[cell] = cell-width;
-        horizontal[cell-width] = 1;
-      }
-      // East
-      else if(wall == 1 && (cell+1)%width != 0){
-        connected[cell] = cell+1;
-        vertical[cell - cell/width] = 1;
-      }
-      // South
-      else if(wall == 2 && (cell+width) < width*height){
-        connected[cell] = cell+width;
-        horizontal[cell] = 1;
-      }
-      // West
-      else if(wall == 3 && cell%width != 0){
-        connected[cell] = cell-1;
-        vertical[cell - cell/width - 1] = 1;
-      }
+        if(wall == 0 && cell >= m_width){
+          unionSets(cell, cell-m_width);
+          horizontal[cell-m_width] = 1;
+        }
+        // East
+        else if(wall == 1 && (cell+1)%m_width != 0){
+          unionSets(cell, cell+1);
+          vertical[cell - cell/m_width] = 1;
+        }
+        // South
+        else if(wall == 2 && (cell+m_width) < m_width*m_height){
+          unionSets(cell, cell+m_width);
+          horizontal[cell] = 1;
+        }
+        // West
+        else if(wall == 3 && cell%m_width != 0){
+          unionSets(cell, cell-1);
+          vertical[cell - cell/m_width - 1] = 1;
+        }
+        else{
+          i--;
+        }
+        
+
+      
 
     }
-    count++;
   }
 
-  // TEST CHECK OUTPUT
-  for(int i = 0; i < numvert; i++){
-    std::cout << vertical[i] << " ";
+  int find(int x) const {
+    if(connected[x] < 0){
+      return x;
+    }
+    else{
+      return find(connected[x]);
+    }
   }
-  std::cout << "\n";
-  for(int i = 0; i < numhor; i++){
-    std::cout << horizontal[i] << " ";
-  }
-  std::cout << "\n";
-  for(int i = 0; i < numconnected; i++){
-    std::cout << connected[i] << " ";
-  }
-  std::cout << "\n\n";
-  // END TEST
 
-
-  int colwidth = 1;
-  for(int i = 0; i < width; i++){
-    std::cout << std::left << std::setw(colwidth) << " _";
-  }
-  std::cout << "\n";
-  for(int i = 0; i < height; i++){
-    std::cout << std::left << std::setw(colwidth) << "|";
-    for(int j = 0; j < width; j++){
-      int cell = j + i*width;
-      
-      if(horizontal[cell] == 1 && cell+width <= height*width){
-        std::cout << std::left << std::setw(colwidth) << "";
-      }
-      else{
-        std::cout << std::left << std::setw(colwidth) << "_";
-      }
-
-      if(vertical[cell - cell/width ] == 1 && (cell+1)%width != 0){
-        std::cout << std::left << std::setw(colwidth) << "";
-      }
-      else{
-        std::cout << std::left << std::setw(colwidth) << "|";
-      }
-            
+  void printvalues(){
+    for(int i = 0; i < numvert; i++){
+      std::cout << vertical[i] << " ";
     }
     std::cout << "\n";
+    for(int i = 0; i < numhor; i++){
+      std::cout << horizontal[i] << " ";
+    }
+    std::cout << "\n";
+    for(int i = 0; i < numconnected; i++){
+      std::cout << connected[i] << " ";
+    }
+    std::cout << "\n\n";
+
   }
-  std::cout << "\n";
 
-  
+  void print(){
+    int colwidth = 1;
+    for(int i = 0; i < m_width; i++){
+      std::cout << std::left << std::setw(colwidth) << " _";
+    }
+    std::cout << "\n";
+    for(int i = 0; i < m_height; i++){
+      std::cout << std::left << std::setw(colwidth) << "|";
+      for(int j = 0; j < m_width; j++){
+        int cell = j + i*m_width;
+
+        if(horizontal[cell] == 1 && cell+m_width <= m_height*m_width){
+          std::cout << std::left << std::setw(colwidth) << "";
+        }
+        else{
+          std::cout << std::left << std::setw(colwidth) << "_";
+        }
+
+        if(vertical[cell - cell/m_width ] == 1 && (cell+1)%m_width != 0){
+          std::cout << std::left << std::setw(colwidth) << "";
+        }
+        else{
+          std::cout << std::left << std::setw(colwidth) << "|";
+        }
+
+      }
+      std::cout << "\n";
+    }
+  }
 
 
-}
+  private:
+  int* connected;
+  int* vertical;
+  int* horizontal;
+  int numvert;
+  int numhor;
+  int numconnected;
+  int m_width;
+  int m_height;
+
+  };
+
+
+  int main(){
+
+    int seed = 0;
+    srand (seed);
+
+    clock_t t0;
+    t0 = clock();
+
+    int width = 5;
+    int height = 5;
+
+
+    DisjSets disset = DisjSets(width, height);
+    disset.print();
+    disset.buildmaze();
+//    std::cout << "find 2: " << disset.find(2) << "\n";
+//    disset.printvalues();
+    disset.print();
+    disset.printvalues();
+
+
+
+
+    std::cout << "\n";
+
+
+
+
+  }
 
