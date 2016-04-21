172a6b8af9784e74945c98025ed349a69bfd48ca
Dustin Horvath <dustinmhorvath@gmail.com>
2016-04-21 13:38:35 -0500

    Has prettier output. Prints number of clock cycles though which might need changed to an actual 'time'.

--- lab12/main.cpp
+++ lab12/main.cpp
@@ -12,11 +12,13 @@
 #include <vector>
 
 void sort(int* array, std::vector<int> increments, int size){
+  /*
   //print before
   for(int print = 0; print < size; print++){
-    std::cout << array[print] << " ";
+  std::cout << array[print] << " ";
   }
   std::cout << "\n";
+  */
 
   for (std::vector<int>::size_type increment = 0; increment != increments.size(); increment++){
     int skip = increments[increment];
@@ -28,12 +30,14 @@ void sort(int* array, std::vector<int> increments, int size){
       }
     }
   }
-  
+
+  /*
   //print after
   for(int print = 0; print < size; print++){
-    std::cout << array[print] << " ";
+  std::cout << array[print] << " ";
   }
-      std::cout << "\n";
+  std::cout << "\n\n";
+  */
 }
 
 
@@ -45,6 +49,7 @@ int main(int argc, char *argv[]){
 
   std::vector<int> increments[4];
 
+  // Prepare vector array of increment values from file
   int linenum = 0;
   while(std::getline(file, line)){
     std::istringstream iss(line);
@@ -58,33 +63,56 @@ int main(int argc, char *argv[]){
     linenum++;
   }
 
-  for(int i = 0; i < 4; i++){
-    for(std::vector<int>::size_type increment = 0; increment != increments[i].size(); increment++){
-  //    std::cout << increments[i][increment] << " ";
-    }
-  //    std::cout << "\n";
-  }
+  // Check increment vectors for testing. Confirmed working.
+  /*
+     for(int i = 0; i < 4; i++){
+     for(std::vector<int>::size_type increment = 0; increment != increments[i].size(); increment++){
+     std::cout << increments[i][increment] << " ";
+     }
+     std::cout << "\n";
+     }
+     */
 
+  int numtypes = 4;
+  int numtrials = 5;
+  int times[numtypes][numtrials];
 
-
-  for(int run = 1; run < 5; run++){
-    int n = run*10;
-    for(int i = 0; i < 5; i++){
-      srand(i);
+  for(int sizerun = 1; sizerun < numtypes + 1; sizerun++){
+    int numtosort = sizerun*100000;
+    for(int trial = 0; trial < numtrials; trial++){
+      srand(trial);
       // to get between -3n and 3n
-      int* array = new int[n];
-      for(int j = 0; j < n; j++){
-        array[j] = rand()%(6*n) - (3*n);
+      int* array = new int[numtosort];
+      for(int j = 0; j < numtosort; j++){
+        array[j] = rand()%(6*numtosort) - (3*numtosort);
       }
       clock_t t0 = clock();
 
-            
-      sort(array, increments[run - 1], n);
-      t0 = clock() - t0;
 
-      
+      sort(array, increments[sizerun - 1], numtosort);
+      times[sizerun][trial] = clock() - t0;
+
+
     }
   }
+
+  int colwidth = 9;
+  std::cout << "\n";
+  std::cout << std::left << std::setw(colwidth) << " " << std::setw(colwidth) << " ";
+  for(int i = 0; i < numtrials; i++){
+    std::cout << std::left << std::setw(colwidth - 2) << "  Trial " << i;
+  }
+  std::cout << "\n";
+  for(int sizerun = 1; sizerun < numtypes + 1; sizerun++){
+    int numtosort = sizerun*100000;
+    std::cout << std::setw(colwidth) << "Size = " << std::left << std::setw(colwidth) << numtosort << ": ";
+    for(int trial = 0; trial < numtrials; trial++){
+      std::cout << std::left << std::setw(colwidth) << times[sizerun][trial];
+
+    }
+    std::cout << "\n";
+  }
+
 
 
 
