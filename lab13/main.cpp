b0978b7cfb5959b6eba5d0a56ed5f475079a21a0
Dustin Horvath <dustinmhorvath@gmail.com>
2016-04-30 19:12:26 -0500

    Fixed up the print function slightly.

--- lab13/main.cpp
+++ lab13/main.cpp
@@ -7,12 +7,15 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <random>
 
 // Used for pretty output
 #include <iomanip>
 #include <vector>
 #include <queue>
 // End pretty libraries
+
+#define MAXNODES 100
 
 class Node{
 private:
@@ -63,11 +66,13 @@ public:
 
 class PairingHeap{
 
+int size;
 Node* elem;
 
 public:
 
   PairingHeap(){
+    size = 0;
     elem = nullptr;
   }
 
@@ -76,14 +81,29 @@ public:
   }
 
   Node* insert(int value){
-    Node* tempnode = new Node(value);
+    Node* temp = new Node(value);
+
     if(elem == nullptr){
-      elem = tempnode;
+      elem = temp;
+      size++;
     }
     else{
-      link(elem, tempnode);
+      if(elem -> getValue() > value){
+        temp -> setLeft(elem);
+        elem -> setPrev(temp);
+        elem = temp;
+        size++;
+      }
+      else{
+        if(elem -> getLeft()){
+          elem -> getLeft() -> setPrev(temp);
+          temp -> setSibling(elem -> getLeft());
+        }
+        elem -> setLeft(temp);
+        temp -> setPrev(elem);
+        size++;
+      }
     }
-    return tempnode;
   }
 
   void link(Node* &first, Node* &second){
@@ -122,79 +142,98 @@ public:
   }
 
   Node* merge(Node* sibling){
+    
     if (sibling -> getSibling() == nullptr){
       return sibling;
     }
-    static std::vector<Node*> treeArray(5);
-    std::queue<Node*> array;
+    
+    Node* nodes[MAXNODES];
+    
     // Count siblings
     int count = 0;
     for (; sibling != nullptr; count++){
-      if (count == treeArray.size()){
-        treeArray.resize(count * 2);
-      }
-      treeArray[count] = sibling;
+      nodes[count] = sibling;
       sibling -> getPrev() -> setSibling(nullptr);
       sibling = sibling -> getSibling();
     }
-    if (count == treeArray.size()){
-      treeArray.resize(count + 1);
-    }
-    treeArray[count] = nullptr;
+    
+    nodes[count] = nullptr;
     int i = 0;
     for (i = 0; i + 1 < count; i += 2){
-      link(treeArray[i], treeArray[i + 1]);
+      link(nodes[i], nodes[i + 1]);
     }
     int j = i - 2;
     if (j == count - 3)
-      link (treeArray[j], treeArray[j + 2]);
+      link (nodes[j], nodes[j + 2]);
     for (; j >= 2; j -= 2)
-      link(treeArray[j - 2], treeArray[j] );
-    return treeArray[0];
+      link(nodes[j - 2], nodes[j] );
+    return nodes[0];
 
   }
 
   void levelorder(){
-    if (elem != nullptr) {
-      std::cout << "Printing level order..." << "\n";
-      int height = 0;
-      Node* temp;
-      std::queue<Node*> siblings;
-      std::queue<Node*> children;
-      siblings.push(elem);
-      while (!siblings.empty() || !children.empty()) {
-        if(!siblings.empty()){
-          temp = siblings.front();
-          siblings.pop();
-          std::cout << temp -> getValue() << " ";
-          if(temp -> getSibling() != nullptr){
-            siblings.push(temp -> getSibling());
-          }
-          if(temp -> getLeft() != nullptr){
-            children.push(temp -> getLeft());
-          }
+    std::queue<Node*> q;
+
+    int level = 0;
+    int nextchildren = 0;
+    int curchildren = 0;
+    Node* current = elem;
+    std::cout << "Level " << level << ": ";
+    while(current != nullptr){
+      // if it has a child, push it on and increment the children in next row
+      if(current -> getLeft()){
+        q.push(current -> getLeft());
+        nextchildren++;
+      }
+      std::cout << current -> getValue() << " ";
+      // if it has a sibling, iterate to it
+      if(current -> getSibling()){
+        current = current -> getSibling();
+      }
+      // if it doesn't have siblings and there are more children in the row, 
+      // get a another child from the row
+      else if(curchildren > 0){
+        std::cout << "    ";
+        curchildren--;
+        current = q.front();
+        q.pop();
+      }
+      // if no more children in row (curchildren == 0), then move down a row
+      // and make curchildren = nextchildren
+      else{
+        // also, this uses up one of the children
+        curchildren = nextchildren;
+        current = q.front();
+        q.pop();
+        // increment our current level
+        level++;
+        std::cout << "\n";
+        if(current){
+          std::cout << "Level " << level << ": ";
         }
-        else if(!children.empty()){
-          std::cout << "\n";
-          temp = children.front();
-          children.pop();
-          std::cout << temp -> getValue() << " ";
-          if(temp -> getSibling() != nullptr){
-            siblings.push(temp -> getSibling());
-          }
-          if(temp -> getLeft() != nullptr){
-            children.push(temp -> getLeft());
-          }
+      }
 
-        }
-
-
+    }
+/*
+    while(current != nullptr){
+      if(current -> getPrev() != nullptr){
+        std::cout << current -> getPrev() -> getValue() << ": ";
       }
       
+      do{
+        std::cout << current -> getValue() << " ";
+        if(current->getLeft()){
+          array[index] = current->getLeft();
+          index++;
+        }
+        current = current->getSibling();
+      }while (current);
+      std::cout << "\n";
+      current = array[head];
+      head++;
     }
-    else {
-      std::cout << "No elements in heap\n";
-    }
+*/
+      
   }
 
 
@@ -207,16 +246,17 @@ public:
 int main(int argc, char *argv[]){
 
   PairingHeap heap = PairingHeap();
-  heap.insert(5);
-  heap.insert(10);
-  heap.insert(18);
-  heap.insert(26);
-  heap.insert(1);
-  heap.insert(90);
-  heap.insert(75);
-  heap.insert(2);
-  heap.insert(5);
-  heap.insert(12);
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(50, 200);
+  int temp;
+
+  for(int i = 0; i < 50; i++){
+    temp = dis(gen);
+    heap.insert(temp);
+  }
+  
   heap.levelorder();
 
 
